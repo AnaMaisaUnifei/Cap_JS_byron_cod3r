f322dc58763380aa68a72f223831477e05b93b5f
Ana Maísa <d2021002575@unifei.edu.br>
2022-03-01 20:51:24 -0300

    Organização e comentários em JS

---
+++ WebModerno_com_JavascriptCOMPLETO+Projetos/exercicios-js/fundamentos/comentario.js
@@ -0,0 +1,19 @@
+/**
+ * Comentários relevantes e necessários (sem repetir o que já existe)
+ * Comentários que fazem sentido com o que está no seu código
+ */
+
+// Comentário de uma linha
+conseloe.log('Linha 1')
+
+/* 
+Comentário de 
+múltiplas linhas
+*/
+consele.log('Linha2')
+
+/*
+* Comentário de
+* Múltiplas linhas
+*/
+console.log('Linha 3')
---
+++ WebModerno_com_JavascriptCOMPLETO+Projetos/exercicios-js/fundamentos/organizacao.js
@@ -0,0 +1,9 @@
+console.log("Sentença de código")
+
+// Blocos de código = agrupar sentenças de código
+{
+  {
+    console.log("Olá")
+    console.log('Mundo!')
+  }
+}
