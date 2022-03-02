ec29c13d9966b573f8f6e34bf12a6fae0d472ac3
Ana Maísa <d2021002575@unifei.edu.br>
2022-03-01 21:32:44 -0300

    Number: Alguns Cuidados

---
+++ WebModerno_com_JavascriptCOMPLETO+Projetos/exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0) //Retorna Infinity
+console.log("10" / 2) // Deus ouviu meus pedidos e não precisarei mais converter, obrigada
+console.log("Show!" * 2) //Não rola = NaN = Not a Number
+console.log (0.1 + 0.7) //Gera imprecisão por ser float
+// console.log(10.toString()) //Não rola = para tudo :(
+console.log((10.345).toFixed(2)) //Funciona
