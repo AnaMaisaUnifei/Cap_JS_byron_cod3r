aad83a200f62264dc5b4715e435f6c90ae9bc0e9
Ana Maísa <d2021002575@unifei.edu.br>
2022-03-01 21:16:32 -0300

    Tipagem fraca

---
+++ WebModerno_com_JavascriptCOMPLETO+Projetos/exercicios-js/fundamentos/tipagemFraca.js
@@ -0,0 +1,12 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// Evite escolher genéricos e siglas (pode fazer sentido pra você, mas pra outra pessoa não)
+let valor = ''
+let numero = 1
+let pqp = false // Pse, P*t@ que Parei (mentira, parei não :) )
