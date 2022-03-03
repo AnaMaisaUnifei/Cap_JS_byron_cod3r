9ac2b07eb16eecd26c5f404c70aecb2d87812e8f
Ana Maísa <d2021002575@unifei.edu.br>
2022-03-03 01:05:22 -0300

    Operadores: Atribuição

---
+++ WebModerno_com_JavascriptCOMPLETO+Projetos/exercicios-js/fundamentos/atribuicao.js
@@ -0,0 +1,19 @@
+const a = 7
+let b = 3
+
+// Símbolo da operação antes e '=' depois :)
+
+b += a // Acrescentando o valor de 'a' a variável b; b = b + a
+console.log(b)
+
+b -= 4 // Subtraindo 4 de  b; b = b - 4
+console.log(b)
+
+b *= 2 // Multiplicando b por 2; b = b * 2
+console.log(b)
+
+b /= 2 // Dividindo b por 2; b = b / 2
+console.log(b)
+
+b %= 2 // Resto da divisão de b por 2; b = b % 2
+console.log(b)
