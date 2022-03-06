2633b7e1a8fbb55d1f4531e25ece04c33d8ac28e
Ana Maísa <d2021002575@unifei.edu.br>
2022-03-06 00:38:37 -0300

    Introdução à OO

---
+++ WebModerno_com_JavascriptCOMPLETO+Projetos/exercicios-js/objeto/oo.js
@@ -0,0 +1,24 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+// Encapsulamento de dados, comportamentos e atributos
+objeto = {
+  valor1,
+  valor2,
+  valor3,
+  processamento(){
+    // ...
+  }
+}
+
+objeto.processamento() // Foco no objeto
+
+/** Princípios importantes
+ * 1. Abstração = pegar um objeto do mundo real e saber traduzi-lo e simplificalo para o programa e adequando as suas necessidades
+ * 2. Encapsulamento = detalhes de implementação escondidos, mostrando apenas o que é necessário para ser usado
+ * 3. Herança (prototype) = reuso de código. Utilizar composições menores dentro de códigos mais complexos. Receber características, atributos e comportamentos de um objeto 'pai'. Em JS, cada extrutura só pode receber um 'pai' e esse pai pode ter outro, o que forma uma cadeia.
+ * 4. Polimorfismo = multiplas formas. Havendo uma variável mais genérica, e nela podem ser referênciadas  coisas que fazem parte da herança do mais genérico (subtipos).
+ */
